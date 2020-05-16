626a69167827ec5164471a8cad136056317cea55
Revan Sarbast <revansarbast8@gmail.com>
2020-05-16 04:04:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+let routine = [
+  function wakeDog() {
+  console.log(`Wake Byron the poodle`);
+},function leashDog() {
+  console.log(`Wake Byron the poodle`);
+},function walkToPark() {
+  console.log(`Wake Byron the poodle`);
+},function throwFrisbee() {
+  console.log(`Wake Byron the poodle`);
+},function walkHome() {
+  console.log(`Wake Byron the poodle`);
+},function unleashDog() {
+  console.log(`Wake Byron the poodle`);
+}]
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+}
